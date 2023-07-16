c3f2a4f954678d1ca55414398aa35b0bb0498e30
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2023-07-15 23:08:32 -0400

    Fix taskly typo and update package metadata

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,5 +2,5 @@
 import { app } from "./app";
 
 // Start the application listening on port 3030.
-console.log("Starting tasky backend on http://0.0.0.0:3030");
+console.log("Starting taskly backend on http://0.0.0.0:3030");
 app.listen(3030);
